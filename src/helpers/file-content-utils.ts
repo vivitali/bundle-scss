85b55b29263c74294647c1a91bd79a8a71606d22
Vitalii <vasinkevych@gmail.com>
2018-04-07 09:07:17 +0300

    remove debug log

--- src/helpers/file-content-utils.ts
+++ src/helpers/file-content-utils.ts
@@ -1,7 +1,6 @@
 import { mainConst } from './constants';
 import { defineExtension, readSync } from './fs-utils';
 import { dirname, join } from 'path';
-import { logger } from './logger';
 
 export const removeImports = (content: string, fileType: string) => {
   const regex =
@@ -15,8 +14,6 @@ export const getUniqueStyleFiles = (
 ) => {
   const imports = files
     .map(file => {
-      logger(file);
-      logger(typeof file);
       let baseDir = dirname(file);
       return getImports(readSync(file), baseDir, fileExtension);
     })
