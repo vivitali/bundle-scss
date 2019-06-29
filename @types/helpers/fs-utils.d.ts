9b2cf5106a39da7ddc28d5b9151e5ee6555f17ff
Vitalii Vasinkevych <vasinkevych@gmail.com>
2019-06-29 17:21:14 +0300

    resolve underscore imports ignore commented imports

--- @types/helpers/fs-utils.d.ts
+++ @types/helpers/fs-utils.d.ts
@@ -2,7 +2,10 @@ export declare const isFile: (f: string) => boolean;
 export declare const readSync: (filePath: string) => string;
 export declare const cwDir: () => string;
 export declare const resolveDirDest: (fileDest: string) => void;
-export declare const writeAsync: (path: string, content: string) => Promise<{}>;
+export declare const writeAsync: (
+  path: string,
+  content: string
+) => Promise<unknown>;
 export declare const fileType: (fileName: string) => string;
 export declare const defineExtension: (
   filePath: string,
--- test/test-sass.js
+++ test/test-sass.js
@@ -5,10 +5,14 @@ const rimraf = require('rimraf');
 
 describe('bundle-scss', () => {
   rimraf.sync('./test/result.sass');
-  it('Should concatenate SASS into one and compare', (done) => {
-    bundleScss(["./**/*.theme.sass"], './test/result.sass').then(f => {
-      assert.equal(f, read('./test/compare.sass', 'utf8'));
-      done()
-    }).catch((err)=>{console.log(err)})
-  })
+  it('Should concatenate SASS into one and compare', done => {
+    bundleScss(['./**/*.theme.sass'], './test/result.sass')
+      .then(f => {
+        assert.equal(f, read('./test/compare.sass', 'utf8'));
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
 });
